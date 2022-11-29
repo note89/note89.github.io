55221f6ba01a3e46ab252ee02fa8cc5078a11e71
Nils Eriksson <nilseriksson89.ne@gmail.com>
2022-11-29 13:00:17 +0000

    add some dataabstraction

--- src/components/Ad/Base.tsx
+++ src/components/Ad/Base.tsx
@@ -1,16 +1,97 @@
 import { StaticImage } from "gatsby-plugin-image"
 import * as React from "react"
+import { format, addWeeks } from "date-fns"
 
 export const Base = () => {
   return <Front />
 }
 
+type Weeks = number
+type CourseParameters = {
+  titleElement: () => JSX.Element
+  link: string
+  startDate: Date
+  runningLength: Weeks
+  highlightElement: () => JSX.Element
+}
+
+class Course {
+  #titleElement: () => JSX.Element
+  #link: string
+  #startDate: Date
+  #runningLength: Weeks
+  #highlightElement: () => JSX.Element
+
+  constructor(c: CourseParameters) {
+    this.#titleElement = c.titleElement
+    this.#link = c.link
+    this.#startDate = c.startDate
+    this.#runningLength = c.runningLength
+    this.#highlightElement = c.highlightElement
+  }
+
+  getTitleElement() {
+    return this.#titleElement
+  }
+  getHighlightElement() {
+    return this.#highlightElement
+  }
+
+  getRunningLengthInWeeks() {
+    return this.#runningLength
+  }
+
+  getHref() {
+    return this.#link
+  }
+
+  getDateElement() {
+    return () => (
+      <>
+        <span>{format(this.#startDate, "do 'of' LLLL")}</span> -{" "}
+        <span>
+          {format(
+            addWeeks(this.#startDate, this.getRunningLengthInWeeks()),
+            "do 'of' LLLL"
+          )}
+        </span>
+      </>
+    )
+  }
+}
+
+const ExpressCourseHighlight = () => <>New offering! 5 weeks, 3h/week</>
+const RegularCourseHighlight = () => <>New cohort starting soon!</>
+const Dates = ({ startDate, weeks }: { startDate: Date; weeks: number }) => (
+  <>
+    <span>{format(startDate, "do 'of' LLLL")}</span> -{" "}
+    <span>{format(addWeeks(startDate, weeks), "do 'of' LLLL")}</span>
+  </>
+)
+
+const Express = () => (
+  <>
+    THE ADVANCED SOFTWARE DESIGN{" "}
+    <span className="text-yellow-400">EXPRESS</span> COURSE
+  </>
+)
+
+const Regular = () => <>THE ADVANCED SOFTWARE DESIGN COURSE</>
+const expressLink =
+  "https://mirdin.com/the-advanced-software-design-express-course/"
+const regularLink = "https://mirdin.com/the-advanced-software-design-course/"
+
+const CurrentCourse = new Course({
+  titleElement: Regular,
+  link: regularLink,
+  startDate: new Date("2022-12-16"),
+  runningLength: 12,
+  highlightElement: RegularCourseHighlight,
+})
+
 const Front = () => {
   return (
-    <a
-      href="https://mirdin.com/the-advanced-software-design-express-course/"
-      target="__blank"
-    >
+    <a href={CurrentCourse.getHref()} target="__blank">
       <div className="mb-8 mx-2 drop-shadow-lg hover:drop-shadow-xl transition ease-in-out duration-400 hover:bg-stone-900 bg-neutral-900 rounded-2xl flex items-center justify-center">
         <div className="flex flex-col text-center p-6">
           <div className="flex flex-col xs:flex-row items-center">
@@ -27,7 +108,7 @@ const Front = () => {
 
             <div>
               <div className="text-left xs:text-right text-sm xs:text-xl text-neutral-200 pb-2 style-font-sans style-bold">
-                THE ADVANCED SOFTWARE DESIGN <span className="text-yellow-400">EXPRESS</span> COURSE
+                {React.createElement(CurrentCourse.getTitleElement())}
               </div>
             </div>
           </div>
@@ -42,10 +123,10 @@ const NextCohort = () => {
   return (
     <h2 className="text-yellow-400 m-0 pb-1 text-sm sm:text-lg text-left">
       <div className="w-full">
-        <div>New offering! 5 weeks, 3h/week</div>
+        <div>{React.createElement(CurrentCourse.getHighlightElement())}</div>
         <div className="flex flex-row items-end justify-between">
           <span className="text-slate-300">
-            <span>21st of November</span> - <span>19th of December</span>
+            {React.createElement(CurrentCourse.getDateElement())}
           </span>
           <span className="text-yellow-400 text-sm text-right ">
             Click to learn more
