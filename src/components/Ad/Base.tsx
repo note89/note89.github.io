ac085f5c053cadadbf6265886bfeb7f8eee7c240
Nils Eriksson <nilseriksson89.ne@gmail.com>
2022-11-29 12:53:03 +0100

    express course ad

--- src/components/Ad/Base.tsx
+++ src/components/Ad/Base.tsx
@@ -8,7 +8,7 @@ export const Base = () => {
 const Front = () => {
   return (
     <a
-      href="https://mirdin.com/the-advanced-software-design-course/"
+      href="https://mirdin.com/the-advanced-software-design-express-course/"
       target="__blank"
     >
       <div className="mb-8 mx-2 drop-shadow-lg hover:drop-shadow-xl transition ease-in-out duration-400 hover:bg-stone-900 bg-neutral-900 rounded-2xl flex items-center justify-center">
@@ -27,7 +27,7 @@ const Front = () => {
 
             <div>
               <div className="text-left xs:text-right text-sm xs:text-xl text-neutral-200 pb-2 style-font-sans style-bold">
-                THE ADVANCED SOFTWARE DESIGN COURSE
+                THE ADVANCED SOFTWARE DESIGN <span className="text-yellow-400">EXPRESS</span> COURSE
               </div>
             </div>
           </div>
@@ -42,10 +42,10 @@ const NextCohort = () => {
   return (
     <h2 className="text-yellow-400 m-0 pb-1 text-sm sm:text-lg text-left">
       <div className="w-full">
-        <div>New cohort</div>
+        <div>New offering! 5 weeks, 3h/week</div>
         <div className="flex flex-row items-end justify-between">
           <span className="text-slate-300">
-            <span>6th of October</span> - <span>19th of December</span>
+            <span>21st of November</span> - <span>19th of December</span>
           </span>
           <span className="text-yellow-400 text-sm text-right ">
             Click to learn more
